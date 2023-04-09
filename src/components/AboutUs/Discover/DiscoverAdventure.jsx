3de83bf2bd4c125aacaf6490d37d294477310eeb
Samuel Santos <samuka0102128@gmail.com>
2023-04-09 12:33:52 -0300

    About Us Responsive

--- src/components/AboutUs/Discover/DiscoverAdventure.jsx
+++ src/components/AboutUs/Discover/DiscoverAdventure.jsx
@@ -6,15 +6,18 @@ import '../AboutUs.css';
 
 export const DiscoverAdventure = () => {
   return (
-    <div>
-      <section className=" text-white flex  flex-col justify-center items-center gap-x-9 lg:w-full lg:flex-row ">
+    <section className=" text-white flex  flex-col justify-center items-center gap-x-8 lg:w-full lg:flex-row ">
+      <div className="m-0 lg:w-2/4 lg:justify-end w-full flex justify-center">
         <YoutubeVideo />
-        <div className="w-3/4 z-50 mt-8 lg:1/4">
-          <h2 className="text-3xl font-semibold mb-7">About Us</h2>
-          <h1 className="text-5xl text-cyan font-semibold mb-7">
-            Discover Your Next Adventure
-          </h1>
-          <p className="lg:text-justify lg:m-0 lg:w-4/4 mb-7 w-full">
+      </div>
+
+      <div className="w-full px-4 z-50 mt-8 lg:w-2/4 lg:justify-center flex justify-start flex-col">
+        <h2 className="text-3xl font-semibold mb-7">About Us</h2>
+        <h1 className="text-5xl text-cyan font-semibold mb-7">
+          Discover Your Next Adventure
+        </h1>
+        <div className="lg:w-3/4 w-full flex justify-center items-center">
+          <p className="text-justify mx-0 mb-7 w-full">
             Our website offers a wide range of travel options, including
             flights, hotels, car rentals, and vacation packages. With our
             easy-to-use interface, you can quickly search and book your travel
@@ -22,10 +25,12 @@ export const DiscoverAdventure = () => {
             a relaxing beach vacation, a thirlling adventure, or a cultural
             immersion, e have the perfect destination for you.
           </p>
+        </div>
+        <div className="w-3/4">
           <Button>View All</Button>
         </div>
-        <GiPaperPlane size={500} color="#ffffff1a" className="paperPlane" />
-      </section>
-    </div>
+      </div>
+      <GiPaperPlane size={500} color="#ffffff1a" className="paperPlane" />
+    </section>
   );
 };
